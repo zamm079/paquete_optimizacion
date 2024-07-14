8fb824926a9373c6d770504b04357bbbef8142d3
zamm079 <100558930+zamm079@users.noreply.github.com>
2024-07-13 18:17:47 -0600

    3

--- funciones_prueba/func_objetivo.py
+++ funciones_prueba/func_objetivo.py
@@ -1,64 +1,90 @@
 import numpy as np
 
-def ackley_function(x, y):
+def ackley_function(X):
+    x=X[0]
+    y=X[1]
     term1 = -20 * np.exp(-0.2 * np.sqrt(0.5 * (x**2 + y**2)))-np.exp(0.5 * (np.cos(2*np.pi * x) + np.cos(2*np.pi * y)))+ 20 + np.exp(1)
     return term1
 
-def beale_function(x,y):
+def beale_function(X):
+    x=X[0]
+    y=X[1]
     r = (1.5 - x + x*y)**2 + (2.25 - x + x*y**2)**2 + (2.625 - x + x*y**3)**2
     return r
 
-def booth_function(X,Y):
+def booth_function(x):
+    X=x[0]
+    Y=x[1]
     r = (X + 2*Y - 7)**2 + (2*X + Y - 5)**2
     return r
 
-def bukin_functionN6(x,y):
+def bukin_functionN6(X):
+    x=X[0]
+    y=X[1]
     r = 100 * np.sqrt(np.abs(y - 0.01*x**2)) + 0.01 * np.abs(x + 10)
     return r
 
-def Crossintray_function(x,y):
+def Crossintray_function(X):
+    x=X[0]
+    y=X[1]
     term1 = np.sin(x) * np.sin(y)
     term2 = np.exp(np.abs(100 - np.sqrt(x**2 + y**2) / np.pi))
     r = -0.0001 * (np.abs(term1 * term2) + 1)**0.1
     return r
 
-def Easom_function(x,y):
+def Easom_function(X):
+    x=X[0]
+    y=X[1]
     r = -np.cos(x)*np.cos(y)*np.exp(-((x-np.pi)**2 + (y-np.pi)**2))
     return r
 
-def Eggholder_function(X,Y):
+def Eggholder_function(x):
+    X=x[0]
+    Y=x[1]
     term1 = -(Y + 47) * np.sin(np.sqrt(np.abs(X/2 + (Y + 47))))
     term2 = -X * np.sin(np.sqrt(np.abs(X - (Y + 47))))
     return term1 + term2
 
-def goldstein_price_function(x,y):
+def goldstein_price_function(X):
+    x=X[0]
+    y=X[1]
     part1 = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2))
     part2 = (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
     r = part1 * part2
     return r
 
-def Himmelblaus_function(x,y):
+def Himmelblaus_function(X):
+    x=X[0]
+    y=X[1]
     r = (x**2 + y - 11)**2 + (x + y**2 -7)**2
     return r
 
-def Holder_table_function(x,y):
+def Holder_table_function(X):
+    x=X[0]
+    y=X[1]
     term1 = -np.abs(np.sin(x)*np.cos(y))
     term2 = np.exp(np.abs(1 - np.sqrt(x**2 + y**2)/np.pi))
     r = term1*term2
     return r
 
-def Levi_functionN13(x,y):
+def Levi_functionN13(X):
+    x=X[0]
+    y=X[1]
     term1 = np.sin(3 * np.pi * x)**2
     term2 = (x - 1)**2 * (1 + np.sin(3 * np.pi * y)**2)
     term3 = (y - 1)**2 * (1 + np.sin(2 * np.pi * y)**2)
     r = term1 + term2 + term3
     return r
 
-def matyas_function(x,y):
+def matyas_function(X):
+    x=X[0]
+    y=X[1]
     r = 0.26 * (x**2 + y**2) - 0.48*x*y
     return r
 
-def McCormick_function(x,y):
+def McCormick_function(X):
+    x=X[0]
+    y=X[1]
     r = np.sin(x + y) + (x - y)**2 - 1.5*x + 2.5*y + 1
     return r
 
@@ -68,23 +94,29 @@ def rastrigin(X):
     r = A * n + np.sum(X**2 - A * np.cos(2 * np.pi * X),axis=0)
     return r
 
-def rosenbrock_funt(X,Y):
+def rosenbrock_funt(x):
+    X=x[0]
+    Y=x[1]
     r = (100 * (Y - X**2)**2) + (1 - X)**2 
     return r
 
-def Schaffer_functionN2(x,y):
+def Schaffer_functionN2(X):
+    x=X[0]
+    y=X[1]
     term1 = np.sin(x**2 - y**2)**2 - 0.5
     term2 = (1 + 0.001 * (x**2 + y**2))**2
     r = 0.5 + term1 / term2
     return r
 
-def Schaffer_functionN4(X,Y):
+def Schaffer_functionN4(x):
+    X=x[0]
+    Y=x[1]
     numerator = np.cos(np.sin(np.abs(X**2 - Y**2)))**2 - 0.5
     denominator = (1 + 0.001 * (X**2 + Y**2))**2
     return 0.5 + numerator / denominator
 
 def shekel(x, a=None, c=None):
-    if a is None:# Esto lo hice para que el usuario pueda meter los pesos que guste, si no se ponen estos
+    if a is None:
         a = np.array([
             [4.0, 4.0, 4.0, 4.0],
             [1.0, 1.0, 1.0, 1.0],
@@ -114,7 +146,9 @@ def StyblinskiTang_function(X):
     r = np.sum(X**4 - 16*X**2 + 5*X,axis=0 )
     return r
 
-def Three_hump_camel_function(X,Y):
+def Three_hump_camel_function(x):
+    X=x[0]
+    Y=x[1]
     r  = 2*X**2 - 1.05*X**4 + (X**6)/6 + X*Y + Y**2
     return r
 
@@ -123,24 +157,24 @@ def Three_hump_camel_function(X,Y):
 
 if __name__ == "__main__":
     puntox = np.array([0.0,0.0])
-    print("prueba ackley_function: ",ackley_function(x=puntox[0],y=puntox[1]))
-    print("prueba beale_function: ",beale_function(x=puntox[0],y=puntox[1]))
-    print("prueba booth_function: ",booth_function(X=puntox[0],Y=puntox[1]))
-    print("prueba bukin_functionN6: ",bukin_functionN6(x=puntox[0],y=puntox[1]))
-    print("prueba Crossintray_function: ",Crossintray_function(x=puntox[0],y=puntox[1]))
-    print("prueba Easom_function: ",Easom_function(x=puntox[0],y=puntox[1]))
-    print("prueba Eggholder_function: ",Eggholder_function(X=puntox[0],Y=puntox[1]))
-    print("prueba goldstein_price_function: ",goldstein_price_function(x=puntox[0],y=puntox[1]))
-    print("prueba Himmelblaus_function: ",Himmelblaus_function(x=puntox[0],y=puntox[1]))
-    print("prueba Holder_table_function: ",Holder_table_function(x=puntox[0],y=puntox[1]))
-    print("prueba Levi_functionN13: ",Levi_functionN13(x=puntox[0],y=puntox[1]))
-    print("prueba matyas_function: ",matyas_function(x=puntox[0],y=puntox[1]))
-    print("prueba McCormick_function: ",McCormick_function(x=puntox[0],y=puntox[1]))
+    print("prueba ackley_function: ",ackley_function(X=puntox))
+    print("prueba beale_function: ",beale_function(X=puntox))
+    print("prueba booth_function: ",booth_function(x=puntox))
+    print("prueba bukin_functionN6: ",bukin_functionN6(X=puntox))
+    print("prueba Crossintray_function: ",Crossintray_function(X=puntox))
+    print("prueba Easom_function: ",Easom_function(X=puntox))
+    print("prueba Eggholder_function: ",Eggholder_function(x=puntox))
+    print("prueba goldstein_price_function: ",goldstein_price_function(X=puntox))
+    print("prueba Himmelblaus_function: ",Himmelblaus_function(X=puntox))
+    print("prueba Holder_table_function: ",Holder_table_function(X=puntox))
+    print("prueba Levi_functionN13: ",Levi_functionN13(X=puntox))
+    print("prueba matyas_function: ",matyas_function(X=puntox))
+    print("prueba McCormick_function: ",McCormick_function(X=puntox))
     print("prueba rastrigin: ",rastrigin(X=puntox))
-    print("prueba rosenbrock_funt: ",rosenbrock_funt(X=puntox[0],Y=puntox[1]))
-    print("prueba Schaffer_functionN2: ",Schaffer_functionN2(x=puntox[0],y=puntox[1]))
-    print("prueba Schaffer_functionN4: ",Schaffer_functionN4(X=puntox[0],Y=puntox[1]))
+    print("prueba rosenbrock_funt: ",rosenbrock_funt(x=puntox))
+    print("prueba Schaffer_functionN2: ",Schaffer_functionN2(X=puntox))
+    print("prueba Schaffer_functionN4: ",Schaffer_functionN4(x=puntox))
     print("prueba shekel: ",shekel(x=puntox))
     print("prueba sphere_function: ",sphere_function(X=puntox))
     print("prueba StyblinskiTang_function: ",StyblinskiTang_function(X=puntox))
-    print("prueba Three_hump_camel_function: ",Three_hump_camel_function(X=puntox[0],Y=puntox[1]))
+    print("prueba Three_hump_camel_function: ",Three_hump_camel_function(x=puntox))
